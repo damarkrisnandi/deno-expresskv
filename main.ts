49ea2c8b4dbd94c30e741b9d795c957a01f9c3cb
damar <damar@gitlab.lab>
2024-10-17 13:42:47 +0700

    add cors

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 // @deno-types="npm:@types/express@4"
 // import express, { NextFunction, Request, Response } from "express";
 import express from "express";
+// @deno-types="npm:@types/cors@^2.8.17"
+import cors from "cors"
 import mainRouter from "./routes/index.ts";
 import usersRouter from "./routes/users.ts";
 
@@ -11,6 +13,7 @@ app.use(express.json());
 
 app.use("/", mainRouter);
 app.use("/users", usersRouter);
+app.use(cors);
 
 app.listen(port, () => {
   console.log(`Listening on ${port} ...`);
